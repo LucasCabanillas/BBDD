d908a87b7bed9a83c714852274ca4c7e0191d248
LucasCabanillas <cabanillasnahuel6@gmail.com>
2024-11-26 02:26:46 -0300

    Actualizacion version alpha

--- CRUD_escuela_privada/BD/conexion.py
+++ CRUD_escuela_privada/BD/conexion.py
@@ -4,40 +4,7 @@ from mysql.connector import Error
 
 class Conexion():
     # #constructor
-    # @staticmethod
-        
-    # def conexionBD():
-    #     config = {
-    #     'host': 'localhost',
-    #     'user': 'root',
-    #     'password': 'root',
-    #     'database': 'escuela_privada'
-    #     }
-    #     try:
-    #         # Crear la conexión
-    #         conexion = mysql.connector.connect(**config)
-    #         print("Conexión exitosa")
-
-    #         # Crear un cursor para ejecutar consultas
-    #         cursor = conexion.cursor()
-
-    #         # Prueba de consulta
-    #         cursor.execute("SHOW TABLES")
-    #         for tabla in cursor:
-    #             print(tabla)
-
-    #     except mysql.connector.Error as e:
-    #         print(f"Error al conectar a MySQL: {e}")
-    #     finally:
-    #         if 'conexion' in locals() and conexion.is_connected():
-    #             #cursor.close()
-    #             #conexion.close()
-    #             print("Conexión ABIERTA") 
-    # conexionBD()
-
-    # Método para conectarse a la base de datos
-    @staticmethod
-
+    @staticmethod #método estático  no requiere acceso a ninguna instancia de la clase ni a sus atributos o métodos
     def conexionBD():
         try:
             # Establecer la conexión
